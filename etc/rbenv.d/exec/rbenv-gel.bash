988cc40883c9c82ab61960791ac868cb18cad4a1
Samuel Cochran <sj26@sj26.com>
2019-04-30 11:24:40 +1000

    Allow disabling gel

--- etc/rbenv.d/exec/rbenv-gel.bash
+++ etc/rbenv.d/exec/rbenv-gel.bash
@@ -1,1 +1,3 @@
-export RUBYLIB="${BASH_SOURCE%/*/*/*/*}/vendor/gel/lib/gel/compatibility:$RUBYLIB"
+if [[ -z "$DISABLE_GEL" ]]; then
+  export RUBYLIB="${BASH_SOURCE%/*/*/*/*}/vendor/gel/lib/gel/compatibility:$RUBYLIB"
+fi
